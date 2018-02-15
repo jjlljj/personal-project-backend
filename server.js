de2e1fcc4473d44f0d2f41fb3fbf576e2fa80a7c
James Logue <logueja@gmail.com>
2018-02-15 16:46:03 -0700

    Pass text to watson call from FE, need to deal with passing response back

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express') 
+const bodyParser = require('body-parser')
 const mockData = require('./src/__mockdata__/mockData')
 const { getToneAnalysis } = require('./src/queries')
 
-
-//const analyzed = getToneAnalysis('a string of text. I hope this works.')
-
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/analyze', (request, response) => {
+// app.post('/analyze', (request, response) => {
+//   response.status(200).json({text: 'analyzed'});
+// })
+
+app.post('/analyze', async (request, response) => {
+  const text = request.body.text
+  const analyzed = await getToneAnalysis(text) 
+  response.setHeader('Content-Type', 'application/json')
   response.status(200).json(analyzed);
 })
 
