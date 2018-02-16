8d99b31832e2348d5366f07bdaee10ab7dbac312
James Logue <logueja@gmail.com>
2018-02-15 18:23:26 -0700

    Move .tone call into server file, pass as response to query

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express') 
 const bodyParser = require('body-parser')
 const mockData = require('./src/__mockdata__/mockData')
-const { getToneAnalysis } = require('./src/queries')
 
+const toneAnalyzer = require('./src/serviceHelper')
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -13,10 +13,26 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/analyze', async (request, response) => {
   const text = request.body.text
-  const analyzed = await getToneAnalysis(text) 
   response.setHeader('Content-Type', 'application/json')
-  response.status(200).json(analyzed);
+
+  toneAnalyzer.tone({ tone_input: text, content_type: 'text/plain'},
+    (err, tone) => {
+      if (err) {
+        console.log(err);
+      } else {
+        response.status(200).json(tone)
+      }
+    }
+  )
+
 })
+
+
+
+
+  
+
+
 
 app.listen(3000, () => {
   console.log('express running localhost3000')
