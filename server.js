33fc51b62b5e4be7a5e186f00a2f90f209c9aa3a
James Logue <logueja@gmail.com>
2018-04-08 15:35:09 -0600

    Add req origin

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000);
 
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://tonedetector.surge.sh/'];
+  const origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 app.post('/analyze', async (request, response) => {
   const text = request.body.text
   response.setHeader('Content-Type', 'application/json')
